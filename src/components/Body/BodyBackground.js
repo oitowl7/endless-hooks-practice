655e620cf972e666e591e3e81020aa1c41d87415
oitowl7 <shear_j@hotmail.com>
2021-11-15 00:07:34 -0500

    Updates

--- src/components/Body/BodyBackground.js
+++
@@ -1,9 +0,0 @@
-function BodyBackground({ url }) {
-  return(
-    <div className="body-img-container">
-      <img src={url} alt="logo" className="body-background"/>
-    </div>
-  )
-}
-
-export default BodyBackground;
